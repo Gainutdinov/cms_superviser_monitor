bc8b9b5ef31ece9dc212b91e4d2911ebf9d9098e
Marat Gainutdinov <Marat.Gainutdinov@icl-services.com>
2018-12-06 18:31:33 +0300

    updated <if> statement.

--- draft.py
+++ draft.py
@@ -30,8 +30,9 @@ class MyThread(QtCore.QThread):
                 self.sleep(10)
                 with open("report2.txt", 'r', encoding='cp1251') as file_handler:
                     self.waitingTime = re.findall('\d+', file_handler.readlines()[6])
-                    if ( len (self.waitingTime)>=3 or    int(self.waitingTime[0])>=30): # how much person waiting on the telephone line
-                        print((self.waitingTime)[0])
+                    print(self.waitingTime)
+                    if (int((self.waitingTime)[0])>=30): # how much person waiting on the telephone line
+                        print((self.waitingTime))
                         if os.path.isfile(QtCore.QDir.current().absoluteFilePath(self.mp3FileName)): # if >= 30 seconds then play Notification.mp3
                             self.playMusic.emit()
                             #print('>=30 seconds')
