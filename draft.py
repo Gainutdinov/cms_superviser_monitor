5e32f3d654529e21436029009e5f409ce71da3d0
Marat Gainutdinov <Marat.Gainutdinov@icl-services.com>
2019-02-18 18:58:45 +0300

    modified time delay and timestamp for monitoring

--- draft.py
+++ draft.py
@@ -21,36 +21,44 @@ class MyThread(QtCore.QThread):
 
     def run(self):
         self.running = True
+        #os.system('copy report.txt report2.txt')
         while self.running:
             if os.path.isfile(QtCore.QDir.current().absoluteFilePath('report.txt')) and os.path.isfile(QtCore.QDir.current().absoluteFilePath('scripted_report.acsup')):
-                if os.name=='posix' or os.name=='Linux':
-                    os.system('cp report.txt report2.txt')
-                else:
-                    os.system('copy report.txt report2.txt')
-                os.system('start "" "scripted_report.acsup"')
-                self.sleep(1)
-                os.system('taskkill /IM acsRep.exe')
-                with open("report2.txt", 'r', encoding='cp1251') as file_handler:
-                    self.waitingTime = re.findall('\d+', file_handler.readlines()[6])
+                #if os.name=='posix' or os.name=='Linux':
+                #    os.system('cp report.txt report2.txt')
+                #else:
+                #    os.system('copy report.txt report2.txt')
+                with open("report.txt", 'r', encoding='cp1251') as file_handler:
+                    self.listOfLines = file_handler.readlines()
+                    self.peopleInQueue = re.findall('\d+', self.listOfLines[4])
+                    self.waitingTime = re.findall('\d+', self.listOfLines[6])
                     print(self.waitingTime)
-                    if (int((self.waitingTime)[0])>=30): # how much person waiting on the telephone line
+                    if (int((self.waitingTime)[0])>=420 or int((self.peopleInQueue)[0])>=7): # how much person waiting on the telephone line
                         print((self.waitingTime))
                         if os.path.isfile(QtCore.QDir.current().absoluteFilePath(self.mp3FileName)): # if >= 30 seconds then play Notification.mp3
-                            #self.playMusic.emit()
+                            self.playMusic.emit()
                             #os.system('cvlc --play-and-exit Notificaion.mp3') for linux
-                            os.system('vlc --play-and-exit -I dummy --dummy-quiet Notificaion.mp3')
-                            print('>=30 seconds')
+                            #os.system('vlc --play-and-exit -I dummy --dummy-quiet ./Notificaion.mp3')
+                            print('>=420 seconds')
                         else:
                             print('There is no ALERT Notificaion.mp3') # nothing to play
                 # remove files after checking the attribute 'Вызовы в очереди'
                 os.remove(QtCore.QDir.current().absoluteFilePath('report.txt'))
-                os.remove(QtCore.QDir.current().absoluteFilePath('report2.txt'))
+                os.system('start "" "scripted_report.acsup"')
+                self.sleep(5)
+                os.system('taskkill /IM acsRep.exe')
                 self.mySignal.emit(str(self.waitingTime[0]))
                 print('Thread started!')
-                self.sleep(20)
+                self.sleep(30)
             else:
+                self.sleep(60)
+                os.system('taskkill /IM acsRep.exe')
+                self.sleep(5)
+                os.system('start "" "scripted_report.acsup"')
                 self.customSignal.emit('Error - File report.txt or scripted_report.acsauto were not found')
-                self.running = False
+                print('self.running = False')
+                datetime=QtCore.QDateTime.currentDateTime()
+                print(datetime.toString())
 
 
 class MyWin(QtWidgets.QMainWindow, Ui_MainWindow):
